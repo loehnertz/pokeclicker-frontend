862eecfa630966f4928b34801d7d7b393729509e
Christian <christian.jac.stuart@gmail.com>
2019-02-14 22:18:33 +0100

    Remove accidentally committed files

--- src/store/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var redux_1 = require("redux");
-var reducers_1 = require("./reducers");
-exports.store = redux_1.createStore(reducers_1["default"]);
-exports["default"] = exports.store;
